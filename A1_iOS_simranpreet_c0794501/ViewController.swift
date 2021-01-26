832d48af2545f3c71756d025cb461ee962dc1ea2
simranaug24 <simranaug24@gmail.com>
2021-01-26 22:57:24 +0530

    run

--- A1_iOS_simranpreet_c0794501/ViewController.swift
+++ A1_iOS_simranpreet_c0794501/ViewController.swift
@@ -10,18 +10,30 @@ import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate{
 
+    @IBOutlet weak var button: UIButton!
     
     @IBOutlet weak var map: MKMapView!
 
+    var aName: CLLocationCoordinate2D!
+    var bName : CLLocationCoordinate2D!
+    var cName: CLLocationCoordinate2D!
+    
+    
     var places = [PLACES]()
     var x = 0
     var locationManager = CLLocationManager() // variable of location manager
+    
+    var destination = CLLocationCoordinate2D()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        map.delegate = self
         map.showsUserLocation = true
+        
+        button.isHidden = true
         
         locationManager.delegate = self  // creating delegate prperty
         
@@ -42,6 +54,56 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
        
     }
     
+    
+    @IBAction func router(_ sender: UIButton)
+    {
+        
+    directionRouter(source: aName, destination: bName)
+    directionRouter(source: bName, destination: cName)
+    directionRouter(source: cName, destination: aName)
+        
+        
+    }
+        func directionRouter(source:CLLocationCoordinate2D,destination:CLLocationCoordinate2D)
+        {
+        map.removeOverlays(map.overlays)
+        let sourcePlaceMark = MKPlacemark(coordinate: source)
+        
+        let destinationPlaceMark = MKPlacemark(coordinate: destination)
+        
+        // request direction
+        let directionRequest = MKDirections.Request()
+        
+        // assign the source and destination properties of they request
+        directionRequest.source = MKMapItem(placemark: sourcePlaceMark)
+        directionRequest.destination = MKMapItem(placemark: destinationPlaceMark)
+        
+        // transportation type
+        directionRequest.transportType = .automobile
+        
+        // calculate the direction
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate { (response, error) in
+            guard  let directionResponse = response else {return}
+            
+            // create the route
+            let route = directionResponse.routes[0] // array
+            
+            // drawing polyline
+            self.map.addOverlay(route.polyline, level: .aboveRoads)
+            
+            // define bounding map rect
+            let rect = route.polyline.boundingMapRect
+            
+            // visibility of the maprect
+            
+           self.map.setVisibleMapRect(rect, edgePadding: UIEdgeInsets(top: 100, left: 100, bottom: 100, right: 100), animated: true)
+            
+         //   self.map.setRegion(MKCoordinateRegion(rect), animated: true)
+        }
+        
+        
+    }
     
     //POLYGON METHOD
     func addPolygon()
@@ -77,6 +139,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         {
         annotation.title = "A"
         annotation.coordinate = coordination
+            aName = coordination
             places.append(PLACES(title:annotation.title, coordinate:annotation.coordinate))
         map.addAnnotation(annotation)
         }
@@ -84,6 +147,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         {
         annotation.title = "B"
         annotation.coordinate = coordination
+            bName = coordination
             places.append(PLACES(title:annotation.title, coordinate:annotation.coordinate))
         map.addAnnotation(annotation)
         }
@@ -91,18 +155,24 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         {
         annotation.title = "C"
         annotation.coordinate = coordination
+            cName = coordination
             places.append(PLACES(title:annotation.title, coordinate:annotation.coordinate))
         map.addAnnotation(annotation)
             addPolygon()
+            button.isHidden = false
         }
         else if x == 4
         {
             
             places.removeAll()
-            map.removeAnnotation(annotation)
-            x==0
+            map.removeAnnotations(map.annotations)
+            map.removeOverlays(map.overlays)
+            x=0
+            button.isHidden = true
         }
         
+        destination = coordination
+      //  button.isHidden = true
     }
 
 
@@ -113,6 +183,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         
         let latitude = userLocation.coordinate.latitude
         let longitude = userLocation.coordinate.longitude
+        displayLoaction(latitude: latitude, longitude: longitude, title: "you are here", subtitle: "")
+        
     }
     func displayLoaction(latitude:CLLocationDegrees,
                          longitude:CLLocationDegrees,
@@ -163,7 +235,9 @@ extension ViewController: MKMapViewDelegate
             return nil
         }
         
-    return nil
+     let marker = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "hlo")
+        marker.tintColor = UIColor.blue
+        return marker
     }
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         
@@ -175,6 +249,15 @@ extension ViewController: MKMapViewDelegate
             rendrer.lineWidth = 4
             
             return rendrer
+        }
+        
+        else if overlay is MKPolyline
+        {
+            let rendrer = MKPolylineRenderer(overlay: overlay)
+            rendrer.lineWidth = 3
+            rendrer.strokeColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            
+            return rendrer 
         }
          return MKOverlayRenderer()
     }
