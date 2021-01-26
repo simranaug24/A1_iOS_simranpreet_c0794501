4c7b45ddc3c969fb2ea8eba168e44e6c4d3edd84
simranaug24 <simranaug24@gmail.com>
2021-01-26 19:57:48 +0530

    map

--- A1_iOS_simranpreet_c0794501/ViewController.swift
+++ A1_iOS_simranpreet_c0794501/ViewController.swift
@@ -6,14 +6,170 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate{
 
+    
+    @IBOutlet weak var map: MKMapView!
+
+    var places = [Place]()
+    var x = 0
+    var locationManager = CLLocationManager() // variable of location manager
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        map.showsUserLocation = true
+        
+        locationManager.delegate = self  // creating delegate prperty
+        
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest  // accuracy of the location
+        
+        
+        locationManager.requestWhenInUseAuthorization()  // REQUEST FOR THE PREMISSION TO ACCESS LOCATION
+        
+        
+        locationManager.startUpdatingLocation()   // start updating the location
+        
+        
+        addDoubleTapGesture()
+        
+        
+        addPolygon ()
+       
+    }
+    
+    func addPolygon()
+    {
+        
+        let coordinates = places.map{$0.coordinate}
+        let polygon = MKPolygon(coordinates: coordinates, count: coordinates.count)
+        map.addOverlay(polygon)
+    }
+    
+    func addAnnotationForPlaces()
+    {
+        map.addAnnotation(places)
+        
+        let overlays = places.map{}
+    }
+    func addDoubleTapGesture ()
+    {
+        let doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(PIN))
+        doubleTapGesture.numberOfTapsRequired = 2
+        
+        map.addGestureRecognizer(doubleTapGesture)
+    }
+    
+    // adding PIN function
+    
+    @objc func PIN (sender: UITapGestureRecognizer)
+    {
+        
+        let touchPoint = sender.location(in: map)
+        let coordination = map.convert(touchPoint, toCoordinateFrom: map)
+        let annotation = MKPointAnnotation()
+       x += 1
+         
+        if x == 1
+        {
+        annotation.title = "A"
+        annotation.coordinate = coordination
+        map.addAnnotation(annotation)
+        }
+        if x == 2
+        {
+        annotation.title = "B"
+        annotation.coordinate = coordination
+        map.addAnnotation(annotation)
+        }
+        if x == 3
+        {
+        annotation.title = "C"
+        annotation.coordinate = coordination
+        map.addAnnotation(annotation)
+        }
+        if x == 4
+        {
+            map.removeAnnotation(annotation)
+        }
+        
     }
 
 
+    // DID update location method
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+    }
+    func displayLoaction(latitude:CLLocationDegrees,
+                         longitude:CLLocationDegrees,
+                         title:String,
+                         subtitle:String)
+    {
+        
+        // define span
+    let  latDelta: CLLocationDegrees = 0.05
+    let lngDelta: CLLocationDegrees = 0.05
+        
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta,
+                                    longitudeDelta: lngDelta)
+        
+        // step 3 define location
+        let location = CLLocationCoordinate2D(latitude: latitude,
+        longitude: longitude)
+        
+        
+        
+        // step 4  define the region
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        // step 5  set the region
+        
+        map.setRegion(region, animated: true)
+        
+        // step 6 add annotation
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = title
+        annotation.subtitle = subtitle
+        annotation.coordinate = location
+        map.addAnnotation(annotation)
+        
+    }
 }
 
+
+extension ViewController: MKMapViewDelegate
+{
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation
+        {
+            return nil
+        }
+        
+    return nil
+    }
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        if overlay is MKPolygon
+        {
+            let rendrer = MKPolygonRenderer(overlay: overlay)
+            rendrer.fillColor = UIColor.red.withAlphaComponent(0.5)
+            rendrer.strokeColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+            rendrer.lineWidth = 4
+            
+            return rendrer
+        }
+         return MKOverlayRenderer()
+    }
+}
