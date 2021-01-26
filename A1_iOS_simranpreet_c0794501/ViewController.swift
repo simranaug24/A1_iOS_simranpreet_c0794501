b65bd9f3e80200968702e9389d8a0d9abd98cb81
simranaug24 <simranaug24@gmail.com>
2021-01-26 17:23:40 +0530

    Initial Commit

---
+++ A1_iOS_simranpreet_c0794501/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_iOS_simranpreet_c0794501
+//
+//  Created by simranPreet KAur on 26/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
